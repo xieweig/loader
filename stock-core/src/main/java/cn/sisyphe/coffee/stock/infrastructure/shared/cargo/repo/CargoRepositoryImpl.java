d81fc4b0ff5053336060067807a7cb4d56de52ae
maoningchuan <minchina91@gmail.com>
2018-02-06 15:17:13 +0800

    避免空指针&不打印sql

--- stock-core/src/main/java/cn/sisyphe/coffee/stock/infrastructure/shared/cargo/repo/CargoRepositoryImpl.java
+++ stock-core/src/main/java/cn/sisyphe/coffee/stock/infrastructure/shared/cargo/repo/CargoRepositoryImpl.java
@@ -44,7 +44,10 @@ public class CargoRepositoryImpl implements CargoRepository {
         // 货物计量
         cargo.setMeasurement(Integer.valueOf(cargoResultMap.get("number").toString()));
         // 货物规格
-        cargo.setMeasurementName(cargoResultMap.get("measurementName").toString());
+        cargo.setMeasurementName("");
+        if (cargoResultMap.get("measurementName") != null) {
+            cargo.setMeasurementName(cargoResultMap.get("measurementName").toString());
+        }
         // 货物标准单位
         cargo.setStandardUnit(cargoResultMap.get("standardUnit").toString());
         cargo.setEffectiveTime(Integer.valueOf(cargoResultMap.get("effectiveTime").toString()));
